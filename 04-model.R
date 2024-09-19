f1faad608f68f9011c07c3ba8a8a0e22bdc09414
jennygzt <jenny.guo@mail.utoronto.ca>
2024-09-19 18:32:05 -0400

    Add files via upload

---
+++ 04-model.R
@@ -0,0 +1,37 @@
+#### Preamble ####
+# Purpose: Modeles for marraige licenses
+# Author: Zitong Guo
+# Date: 19 September, 2024
+# Contact: jenny.guo@mail.utoronto.ca 
+# License: MIT
+# Pre-requisites: 
+# Any other information needed? None
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+
+#### Read data ####
+analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+
+### Model data ####
+first_model <-
+  stan_glm(
+    formula = flying_time ~ length + width,
+    data = analysis_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 853
+  )
+
+
+#### Save model ####
+saveRDS(
+  first_model,
+  file = "models/first_model.rds"
+)
+
+
